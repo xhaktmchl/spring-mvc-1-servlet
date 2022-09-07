b451675276560dc3e09a352f31649796a79d6b35
xhaktmchl <xhaktmchl@naver.com>
2022-09-07 20:20:07 +0900

    feat: 프론트 컨트롤러 구조도입 V1

---
+++ src/main/java/hello/springmvc1servlet/web/frontcontroller/v1/controller/MemberSaveControllerV1.java
@@ -0,0 +1,35 @@
+package hello.springmvc1servlet.web.frontcontroller.v1.controller;
+
+import hello.springmvc1servlet.domain.member.Member;
+import hello.springmvc1servlet.domain.member.MemberRepository;
+import hello.springmvc1servlet.web.frontcontroller.v1.ControllerV1;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MemberSaveControllerV1 implements ControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        System.out.println("member = " + member);
+        memberRepository.save(member);
+
+        //Model에 데이터를 보관한다.
+        request.setAttribute("member", member);
+
+        // 저장 결과 jsp 호출
+        String viewPath = "/WEB-INF/views/save-result.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
