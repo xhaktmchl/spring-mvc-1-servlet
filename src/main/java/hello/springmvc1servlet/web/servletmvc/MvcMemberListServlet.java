964f8193a4564df3b62c22fccf23a415b7033452
xhaktmchl <xhaktmchl@naver.com>
2022-09-07 17:10:55 +0900

    feat: jsp-servlet 으로 MVC 패턴 회원목록 조회 구현

---
+++ src/main/java/hello/springmvc1servlet/web/servletmvc/MvcMemberListServlet.java
@@ -0,0 +1,31 @@
+package hello.springmvc1servlet.web.servletmvc;
+
+import hello.springmvc1servlet.domain.member.Member;
+import hello.springmvc1servlet.domain.member.MemberRepository;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "mvcMemberListServlet", urlPatterns = "/servlet-mvc/members")
+public class MvcMemberListServlet extends HttpServlet {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("MvcMemberListServlet.service");
+        List<Member> members = memberRepository.findAll();
+
+        // Model 에 데이터 저장
+        request.setAttribute("members", members);
+
+        // 회원 전체조회 jsp 호출
+        String viewPath = "/WEB-INF/views/members.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
